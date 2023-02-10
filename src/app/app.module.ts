fff39d0379077087059887f2a72bbe9a9105f6f1
Tfragao <fragaot@gmail.com>
2023-02-10 21:13:17 +0100

    App ready for deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent
+    ArticleComponent,
+   
   ],
   imports: [
     BrowserModule
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import { Article } from './article.model';
+
 
 @Component({
   selector: 'app-article',
@@ -8,21 +9,20 @@ import { Article } from './article.model';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-
-  article: Article;
-
+  @Input()
+  article!: Article; 
   constructor() {
-    this.article = new Article('Angular', 'http://angular.io', 10);
+    
   }
 
   ngOnInit(): void {}
 
-  voteUp() : boolean {
+  voteUp(): boolean {
     this.article.voteUp();
-    return false;  //This tells the browswer to no progate this event to his parent
+    return false; //This tells the browswer to no progate this event to his parent
   }
 
-  voteDown() : boolean {
+  voteDown(): boolean {
     this.article.voteDown();
     return false;
   }
